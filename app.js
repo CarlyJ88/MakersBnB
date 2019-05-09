41e08d220dc757ee13b05c94a2122e5ce05efa97
Carly <carly.jenkinson65@googlemail.com>
2019-05-09 16:27:04 +0100

    changed Login and Sign from from button to anchor to allow easy transition to Sign Up and Log in pages.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,10 +28,38 @@ app.use('/users', usersRouter);
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'Test',
-    items: toDoItems
+
   });
 });
+
+app.get('/SignUp', function (req, res) {
+  res.render('SignUp', {
+
+  });
+})
+
+app.get('/LogIn', function (req, res) {
+  res.render('LogIn', {
+
+  });
+})
+
+// app.post('/', function (req, res) {
+//   // if (SignUp) {
+//   //   console.log("Please sign up");
+//   //   return res.redirect('/SignUp');
+//   // }
+//   // else if (LogIn) {
+//   //   console.log("Please sign in");
+//   //   return res.redirect('/LogIn');
+//   // }
+// });
+
+//   app.post('/LogIn', function (req, res) {
+//     var newItem = req.body.newItem;
+
+//   res.redirect('/LogIn');
+// });
 
 
 app.listen(3000, function () {
